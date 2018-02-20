e5d8ca249e3d9f81444ce7f9dc125909f3a953b1
Mikel Bober-Irizar <mikel@mxbi.net>
2018-02-20 23:10:42 +0000

    Add precise movement based on rotary encoders
    It's possible this maths may break entirely when moving backwards, I haven't checked.

--- src/librgs.py
+++ src/librgs.py
@@ -2,35 +2,84 @@ import numpy as np
 import time
 import copy
 
+# Define units
+ms = 0.001
+
+# Define constants
+RE_PER_CM = 11.13       # Rotary encodes per cm moved - ~1% error
+RE_LATENCY = 60 * ms # Rotary encoder function lag in milliseconds - currently pulled out of my ass
+RE_PREDICT_TIME = 100 * ms # Stop checking rotary encoder and use the current velocity to predict when this many seconds remain
+
+FAST_MOVE_SPEED = 1
+
+def wait_until(time):
+    "Wait until `time` in unix epoch time"
+    time.sleep(time.time() - time)
+
 class RobotController:
     def __init__(self, robot):
         self.r = robot
         self.mleft = self.r.motor_board.m0
         self.mright = self.r.motor_board.m1
 
-        self.ramp_up_speed = 0.05 # Speed increase allowed every 10ms
-        self.ramp_down_speed = 0.1 # Speed decrease allowed every 10ms
+        self.arduino = self.servo_board
 
-    def _set_speed_both(self, speed):
-        self.mleft, self.mright = speed, speed
+        self._update_re()
 
-    def _get_speed_both(self):
+    def _update_re(self):
+        self.left_re, self.right_re = self.arduino.direct_command('r')
+        self.re = np.array([self.left_re, self.right_re])
+        self.re_time = time.time() - RE_LATENCY # Estimate actual time measurement was taken
+
+    @property
+    def speed(self):
         return [self.mleft, self.mright]
 
-    # def _ramp_to_speed(self, target_speed, set_func=self._set_speed_both, get_func=self._get_speed_both):
-    #     assert motor in ['left', 'right', 'both']
-    #     set_func = {'left': self._set_speed_left, 'right': self._set_speed_right, 'both': self.set_speed_both}[motor]
-    #
-    #     current_speed = get_speed(both)
-    #     while motor < target_speed:
-    #         t0 = time.time()
-    #         for motor in
-    #             if motor + self.ramp_up_speed >= target_speed: # If the speed is less than one step away from reaching the target, go
-    #                 set_func(target_speed)
-    #                 break
-    #             else: # Otherwise, slowly ramp up the speed
-    #                 motor += self.ramp_up_speed
-    #
-    #         time.sleep(0.01 - (time.time() - t0)) # Sleep 10ms minus the time spent in the loop for better timing control
-    #
-    # def _ramp_to_speed(self, motors, )
+    @speed.setter # Dark magic, when "self.speed = 1" is called, update both motors
+    def speed(self, speed):
+        self.mleft, self.mright = speed
+
+    def move(self, distance, movement_speed=FAST_MOVE_SPEED):
+        "Accurately move `distance` cm using rotary encoders. Can be negative"
+        # TODO: Actively correct for drift during movement
+        self._update_re()
+        initial_re = self.re.copy()
+        initial_time = self.re_time
+
+        if distance > 0:
+            self.speed = movement_speed
+        else:
+            self.speed = -movement_speed
+
+        while True:
+            # Save last rotary encoder values for comparison
+            old_re = self.re.copy()
+            old_re_time = self.re_time
+
+            self._update_re()
+
+            # Note this is a vector because re/old_re is a vector
+            total_distance = (self.re - initial_re) / RE_PER_CM
+
+            re_time_delta = self.last_re_time - old_re_time
+            velocity = (self.re - old_re) / RE_PER_CM / re_time_delta
+
+            # TODO: How to deal with vectors? Right now I just work based on the left rotary encoder
+            time_remaining = (distance - total_distance[0]) / velocity[0]
+            # Because "re_time" is corrected for the latency of the re function, this pseudo-works out the time when the movement should actually finish,
+            # not when the rotary encoder says the movement should finish (which would be 60ms or so off)
+            end_time = self.re_time + time_remaining
+
+            print('[RobotController] Distance travelled {}cm Velocity {}cm/s Time remaining {}s'.format(total_distance, velocity, time_remaining))
+
+            if time_remaining < RE_PREDICT_TIME:
+                break
+
+        wait_until(end_time)
+        self.speed = 0
+
+        self._update_re()
+
+        print('[RobotController] Finished - travelled {}cm'.format(self.re))
+
+        return self.re
