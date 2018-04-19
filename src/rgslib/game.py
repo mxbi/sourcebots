b86bada7f3c4c231db7901814b6ae2accdf73396
Mikel Bober-Irizar <mikel.cat7@gmail.com>
2018-04-19 18:48:14 +0100

    More code
    Co-Authored-By: Salvatore Elia <salvatore@totietje.net>

--- src/rgslib/game.py
+++ src/rgslib/game.py
@@ -9,7 +9,7 @@ from . import VISION_DISTANCE_FACTOR
 
 class GameState:
 	"""GameState calculates and stores estimates locations of all objects of interest in the game as well as localising the robot itself."""
-	def __init__(self, friendly_zone):
+	def __init__(self, friendly_zone, trig_markers=['COLUMN', 'WALL']):
 		try:
 			from robot import WALL, COLUMN, TOKEN_ZONE_0, TOKEN_ZONE_1, TOKEN_ZONE_2, TOKEN_ZONE_3
 		except ImportError:
@@ -28,6 +28,8 @@ class GameState:
 
 		self.init_time = time.time()
 		self.vision_waits = 0
+
+		self.useful_marker_list = trig_markers
 
 		self.pillars = [
 			util.Rectangle(381, 419, 181, 219),
@@ -55,11 +57,14 @@ class GameState:
 
 		# Minimum distance to avoid the edges of pillars by, in cm
 		# Should be roughly equal to width of robot so if center avoids expanded zone, then the sides will avoid the pillars
-		min_edge_distance = 40
+		min_edge_distance = 20
 		self.zones_to_avoid = [pillar.expand(min_edge_distance) for pillar in self.pillars]
 
 		# Edges of zones we want to avoid
 		self.edges_to_avoid = [edge for pillar in self.pillars for edge in pillar.edges]
+
+	def start(self):
+		self.init_time = time.time()
 
 	def _init_wall_positions(self):
 		# (x, y) of all the wall markers - see https://docs.sourcebots.co.uk/rules.pdf
@@ -163,7 +168,7 @@ class GameState:
 			return
 
 		self.vision_updates += 1
-		useful_markers = [m for m in markers if self.get_marker_type(m) in ['WALL', 'COLUMN']]
+		useful_markers = [m for m in markers if self.get_marker_type(m) in self.useful_marker_list]
 		if len(useful_markers) == 0:
 			if verbose:
 				print('[GameState] No anchored markers')
